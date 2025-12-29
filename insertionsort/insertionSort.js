72e8ece0b000071c832c9419ece2689f923d5d7d
montif16 <Lukas1532@hotmail.com>
2025-12-29 18:13:10 +0100

    implement insertion sort function and playground for testing. None functional for sorting purporses for now.

---
+++ insertionsort/insertionSort.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export function insertionSort(arr) {
+  let iterations = 0;
+
+  return {
+    arr,
+    iterations,
+    algorithm: "insertion sort",
+    sorted: false
+  };
+}
---
+++ insertionsort/playground.js
@@ -0,0 +1,5 @@
+import { insertionSort } from "./insertionSort.js";
+
+const list = [5, 8, 2, 1, 0, 4, 3, 9, 7, 6];
+
+console.log(insertionSort(list));
