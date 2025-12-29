02d858162e680502c20ad1ea1b318e0474362896
montif16 <Lukas1532@hotmail.com>
2025-12-29 23:59:23 +0100

    refactor

--- insertionsort/insertionSort.js
+++ insertionsort/insertionSort.js
@@ -1,12 +1,24 @@
-"use strict";
-
 export function insertionSort(arr) {
   let iterations = 0;
 
+  for (let i = 1; i < arr.length; i++) {
+    iterations++;
+
+    const key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      iterations++;
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+
+      // Sorted array and number of iterations
+    // Insertion sort (shift)
   return {
-    arr,
-    iterations,
-    algorithm: "insertion sort",
-    sorted: false
+    arr, iterations
   };
 }
